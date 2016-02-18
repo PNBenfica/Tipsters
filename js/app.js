d8bdfa7757bfb6590d6d5e7abccd310bb776d306
Paulo Nuno Cainco Teixeira <ist175449@sigma03.ist.utl.pt>
2016-02-18 12:46:27 +0000

    initial commit. interface base project

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//Define an angular module for our app
+var tipstersApp = angular.module('tipstersApp', []);
+ 
+//Define Routing for app
+tipstersApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/feed.html',
+        controller: 'FeedController'
+    }).
+      when('/Rankings', {
+        templateUrl: 'partials/rankings.html',
+        controller: 'RankingsController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,22 @@
+
+var tipstersApp = tipstersApp || {};
+
+
+tipstersApp.run( function($rootScope, $location) {
+   	$rootScope.$watch(function() { 
+      return $location.path(); 
+    },
+    function(a){  
+      $rootScope.$broadcast('urlChange');
+    });
+});
+
+
+tipstersApp.controller('FeedController', function($scope) {
+     
+});
+ 
+ 
+tipstersApp.controller('RankingsController', function($scope) {
+ 
+});
